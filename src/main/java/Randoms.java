b5e90583b00d27550298a109a367e2f2562f3730
SokolovNetology <sokolov180375@mail.ru>
2024-02-29 16:52:53 +0300

    endless iteration

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,40 @@
+
+import java.util.Iterator;
+import java.util.Random;
+public class Randoms implements Iterable<Integer> {
+
+    private final Random random;
+
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+        //...
+    }
+
+    //...
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator <Integer> (){
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                int dif = (max - min)+2;
+                int nextInt = random.nextInt(dif + 1);
+                nextInt += min;
+                return nextInt;
+
+            }
+
+
+        };
+    }
+}
